cfd9ef4a6ffdc26d88346fef5adee2a3d4df4cdf
Petcu Cosmin <petcucosmin.contact@gmail.com>
2023-10-28 03:36:52 +0300

    Visual redesign, mobile capabile
    Made the interface look better and now the control panel is mobile friendly

--- admincontrolpanel/components/CarForm.js
+++ admincontrolpanel/components/CarForm.js
@@ -104,13 +104,15 @@ export default function CarForm({
                     ))}
                 </select>
                 {propertiesToFill.length > 0 && propertiesToFill.map(p => (
-                    <div key={p._id} className="flex gap-1">
-                        <div>{p.name}</div>
-                        <select value={productProperties[p.name]} onChange={ev => setProductProp(p.name,ev.target.value)}>
-                            {p.values.map(v => (
-                                <option key={v._id} value={v}>{v}</option>
-                            ))}
-                        </select>
+                    <div key={p._id} className="">
+                        <label>{p.name[0].toUpperCase()+p.name.substring(1)}</label>
+                        <div>
+                            <select value={productProperties[p.name]} onChange={ev => setProductProp(p.name,ev.target.value)}>
+                                {p.values.map(v => (
+                                    <option key={v._id} value={v}>{v}</option>
+                                ))}
+                            </select>
+                        </div>
                     </div>
                 ))}
                 <label>
@@ -119,7 +121,7 @@ export default function CarForm({
                 <div className="mb-2 flex flex-wrap gap-1">
                     <ReactSortable list={images} className="flex flex-wrap gap-1" setList={updateImagesOrder}>
                         {!!images?.length && images.map(link => (
-                            <div key={link} className="h-24">
+                            <div key={link} className="h-24 bg-white p-4 shadow-sm rounded-sm border border-gray-200">
                                 <img src={link} alt="" className="rounded-lg"/>
                             </div>
                         ))}
@@ -130,13 +132,13 @@ export default function CarForm({
                         </div>
                     )}
                     <label className="w-24 h-24 text-center cursor-pointer
-                    flex items-center justify-center text-sm gap-1
-                    text-gray-500 rounded-lg bg-gray-200">
+                    flex flex-col items-center justify-center text-sm gap-1
+                    text-primary rounded-sm bg-white shadow-sm border border-primary">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5" />
                         </svg>
                         <div>
-                            Upload
+                            Add Image
                         </div>
                         <input type="file" onChange={uploadImages} className="hidden"/>
                     </label>
--- admincontrolpanel/components/Nav.js
+++ admincontrolpanel/components/Nav.js
@@ -1,10 +1,13 @@
 import Link from "next/link";
 import {useRouter} from "next/router";
 import {signOut} from "next-auth/react";
+import Logo from "@/components/Logo";
 
-export default function Nav() {
+export default function Nav({show}) {
     const inactiveLink = 'flex gap-1 p-1';
-    const activeLink = inactiveLink+' bg-white text-blue-900 rounded-l-lg';
+    const activeLink = inactiveLink+' bg-highlight text-black rounded-lg';
+    const inactiveIcon = 'w-6 h-6';
+    const activeIcon = inactiveIcon + ' text-primary';
     const router = useRouter();
     const {pathname} = router;
     async function logout() {
@@ -12,37 +15,32 @@ export default function Nav() {
         await signOut();
     }
     return (
-        <aside className="text-white p-4 pr-0">
-            <Link href={'/'} className="flex gap-1 mb-4 mr-4">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 18.75a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m3 0h6m-9 0H3.375a1.125 1.125 0 01-1.125-1.125V14.25m17.25 4.5a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m3 0h1.125c.621 0 1.129-.504 1.09-1.124a17.902 17.902 0 00-3.213-9.193 2.056 2.056 0 00-1.58-.86H14.25M16.5 18.75h-2.25m0-11.177v-.958c0-.568-.422-1.048-.987-1.106a48.554 48.554 0 00-10.026 0 1.106 1.106 0 00-.987 1.106v7.635m12-6.677v6.677m0 4.5v-4.5m0 0h-12" />
-                </svg>
-                <span className="">
-                    Admin MasiniSH
-                </span>
-            </Link>
+        <aside className={(show?'left-0':'-left-full') + " top-0 text-gray-500 p-4 fixed w-full bg-bgGray h-full md:static md:w-auto transition-all"}>
+            <div className="mb-4 mr-4">
+                <Logo />
+            </div>
             <nav className="flex flex-col gap-2">
                 <Link href={'/'} className={pathname === '/' ? activeLink : inactiveLink}>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={pathname === '/' ? activeIcon : inactiveIcon}>
                         <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
                     </svg>
                     Dashboard
                 </Link>
                 <Link href={'/cars'} className={pathname.includes('/cars') ? activeLink : inactiveLink}>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={pathname === '/cars' ? activeIcon : inactiveIcon}>
                         <path strokeLinecap="round" strokeLinejoin="round" d="M20.25 7.5l-.625 10.632a2.25 2.25 0 01-2.247 2.118H6.622a2.25 2.25 0 01-2.247-2.118L3.75 7.5M10 11.25h4M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125z" />
                     </svg>
                     Cars
                 </Link>
                 <Link href={'/categories'} className={pathname.includes('/categories') ? activeLink : inactiveLink}>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={pathname === '/categories' ? activeIcon : inactiveIcon}>
                         <path strokeLinecap="round" strokeLinejoin="round" d="M9.568 3H5.25A2.25 2.25 0 003 5.25v4.318c0 .597.237 1.17.659 1.591l9.581 9.581c.699.699 1.78.872 2.607.33a18.095 18.095 0 005.223-5.223c.542-.827.369-1.908-.33-2.607L11.16 3.66A2.25 2.25 0 009.568 3z" />
                         <path strokeLinecap="round" strokeLinejoin="round" d="M6 6h.008v.008H6V6z" />
                     </svg>
                     Categories
                 </Link>
                 <Link href={'/settings'} className={pathname.includes('/settings') ? activeLink : inactiveLink}>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={pathname === '/settings' ? activeIcon : inactiveIcon}>
                         <path strokeLinecap="round" strokeLinejoin="round" d="M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.324.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 011.37.49l1.296 2.247a1.125 1.125 0 01-.26 1.431l-1.003.827c-.293.24-.438.613-.431.992a6.759 6.759 0 010 .255c-.007.378.138.75.43.99l1.005.828c.424.35.534.954.26 1.43l-1.298 2.247a1.125 1.125 0 01-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.57 6.57 0 01-.22.128c-.331.183-.581.495-.644.869l-.213 1.28c-.09.543-.56.941-1.11.941h-2.594c-.55 0-1.02-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 01-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 01-1.369-.49l-1.297-2.247a1.125 1.125 0 01.26-1.431l1.004-.827c.292-.24.437-.613.43-.992a6.932 6.932 0 010-.255c.007-.378-.138-.75-.43-.99l-1.004-.828a1.125 1.125 0 01-.26-1.43l1.297-2.247a1.125 1.125 0 011.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.087.22-.128.332-.183.582-.495.644-.869l.214-1.281z" />
                         <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
                     </svg>
